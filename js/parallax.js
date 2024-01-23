15862d90d269fb5d59e9640455f31a3773ce2099
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-01-23 14:38:23 +0200

    jobs

--- js/parallax.js
+++
@@ -1,7 +0,0 @@
-// parallax.js
-
-document.addEventListener("scroll", function() {
-    var scrollPosition = window.scrollY;
-    document.getElementById("parallax").style.transform = "translateY(" + scrollPosition * 0.1 + "px)";
-  });
-  
--- js/slider.js
+++ js/slider.js
@@ -14,3 +14,39 @@ $(document).ready(function(){
       // Другие настройки по вашему выбору
     });
   });
+
+  //--------Aboutus slider--------
+  $(document).ready(function(){
+    $('.feedback-img').slick({
+      slidesToShow: 3, // Количество отображаемых слайдов
+      slidesToScroll: 1,
+      infinite: true,
+      speed: 700,
+      arrows: true,
+      dots: true,
+      centerMode: true,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      responsive: [
+        {
+          breakpoint: 1440, // Свойства для min-width: 1440px
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            variableWidth: true // Отключаем variableWidth для уменьшения размеров слайдов
+          }
+        },
+        {
+          breakpoint: 768, // Свойства для min-width: 768px
+          settings: {
+            slidesToShow: 1,
+            centerMode: false,
+            variableWidth: false
+          }
+        }
+        // Можете добавить дополнительные настройки для других разрешений
+      ]
+    });
+  });
+  
