d8f81eb63189a49c8d7dd2410200c2af7591b3d4
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-01-25 15:07:04 +0200

    адапт верстка Головна

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,41 @@
 $(document).ready(function(){
-    $('.ourworks').slick({
-      // Настройки слайдера
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      infinite: true,
-      speed: 700,
-      arrows: true,
-      dots: true,
-      centerMode: true,
-      variableWidth: true,
-      autoplay: true,
-      autoplaySpeed: 1500
-      // Другие настройки по вашему выбору
-    });
+  $('.ourworks').slick({
+    // Настройки слайдера
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 700,
+    arrows: true,
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    // Другие настройки по вашему выбору
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      }
+    ]
   });
+});
+
+
 
   //--------Aboutus slider--------
   $(document).ready(function(){
