12f630282c97d97f85a6a1cc9b9c2668f319ebfd
MaksymChukhrai <106784292+MaksymChukhrai@users.noreply.github.com>
2024-01-29 03:37:12 +0200

    PHP mailer added

---
+++ js/script-new.js
@@ -0,0 +1,80 @@
+console.log('Init!');
+
+// inputmask
+const form = document.querySelector('.form');
+const telSelector = form.querySelector('input[type="tel"]');
+const inputMask = new Inputmask('+38 (999) 999-99-99');
+inputMask.mask(telSelector);
+
+const validation = new JustValidate('.form');
+
+validation
+  .addField('.input-name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      value: true,
+      errorMessage: 'Введіть ім"я!'
+    }
+  ])
+  // Не забудь убрать инпуты для e-mail с модальных окон!!!!!!!!!!!
+  // .addField('.input-mail', [
+  //   {
+  //     rule: 'required',
+  //     value: true,
+  //     errorMessage: 'Email обов"язковий',
+  //   },
+  //   {
+  //     rule: 'email',
+  //     value: true,
+  //     errorMessage: 'Введіть коректний Email',
+  //   },
+  // ])
+  .addField('.input-tel', [
+    {
+      rule: 'required',
+      value: true,
+      errorMessage: 'Телефон обовя"зковий',
+      
+    },
+    {
+      rule: 'function',
+      validator: function() {
+        const phone = telSelector.inputmask.unmaskedvalue();
+        return phone.length === 10;
+      },
+      errorMessage: 'Введіть коректний телефон',
+    },
+  ]).onSuccess((event) => {
+    console.log('Validation passes and form submitted', event);
+
+    let formData = new FormData(event.target);
+
+    console.log('Form data:', formData);
+    console.log('Form data as array:', Array.from(formData.entries()));
+
+    console.log('Form data:', formData);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('Відправлено');
+          // здесь можно добавить alert окно
+        }
+      }
+    }
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.send(formData);
+
+    event.target.reset();
+  });
